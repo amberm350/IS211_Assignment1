0534bd3665a8d19c74b7b7f33945151aa74a3a2f
amberm350 <112363236+amberm350@users.noreply.github.com>
2022-09-07 22:41:40 -0400

    Add files via upload

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -1,23 +1,29 @@
+class ListDivideException(Exception):
+    pass
+
 def listDivide(numbers,divide = 2):
+    if divide == 0:
+        raise ListDivideException ("Trying to divide by 0")
+
     answer = 0
     for x in numbers:
-        if x % divide != 0:
-            pass
-        else: 
+        if x % divide == 0:
             answer += 1
+
     return answer
 
-class ListDivideException(Exception):
-    Exception = ("Test failed")
+
 
 def testlistDivide():
 
     assert listDivide([1,2,3,4,5]) == 2
     assert listDivide([2,4,6,8,10]) == 5
-    assert listDivide([30, 54, 63,98, 101], divide=100) == 2
+    assert listDivide([30, 54, 63,98, 100], divide=10) == 2
     assert listDivide([]) == 0
     assert listDivide([1,2,3,4,5], 1) == 5
 
-    raise Exception
     
+if __name__ == "__main__":
+    testlistDivide()
+
     
